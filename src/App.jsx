03d908bd009cea2dc25eae9d0f2b96650e0559f5
koheisugawra <51276845+suzy1031@users.noreply.github.com>
2021-09-22 23:57:12 +0900

    import App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
-import SearchContainer from './components/SearchContainer';
+// import Header from './components/Header';
+// import SearchContainer from './components/SearchContainer';
+
+import { Provider } from 'react-redux';
+import store from './lib/redux';
+
+import InboxScreen from './components/InboxScreen';
+import './index.css';
+
+// function App() {
+//   return (
+//     <div className='App'>
+//       <Header />
+//       <SearchContainer />
+//     </div>
+//   );
+// }
 
 function App() {
   return (
-    <div className='App'>
-      <Header />
-      <SearchContainer />
-    </div>
+    <Provider store={store}>
+      <InboxScreen />
+    </Provider>
   );
 }
 
